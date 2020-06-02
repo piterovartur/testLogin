93e59d3f81c1f8d31044216b9a28c129d677e0bc
Artur <piterovartur@yandex.ru>
2020-06-02 18:15:59 +0400

    Добавлены два пользователя

--- js/main.js
+++ js/main.js
@@ -1,32 +1,40 @@
 let array = {
-	artur: {
-		name: "Артур",
-		password: "1h8nn3n6"
-	},
-	bogdan: {
-		name: "Богдан",
-		password: "3h3nn3n3"
-	}
+    artur: {
+        name: "Артур",
+        password: "1h8nn3n6"
+    },
+    bogdan: {
+        name: "Богдан",
+        password: "3h3nn3n3"
+    },
+    ruslan: {
+        name: "Руслан",
+        password: "121323434"
+    },
+    caroline: {
+        name: "Кэролайн",
+        password: "wzgn82"
+    }
 };
 
 let answer = confirm('Войти?');
 
 if (answer) {
-	while (true) {
-		let login = prompt("Введите логин: ");
-		if (login in array) {
-			while (true) {
-				let password = prompt("Введите пароль: ");
-				if (password == array[login]["password"]) {
-					alert('Вы успешно вошли на страницу!');
-					break;
-				} else {
-					alert('Неправильный пароль!');
-				}
-			} break;
-		} else {
-			alert('Такого пользователя не существует.');
-		}
-	}
+    while (true) {
+        let login = prompt("Введите логин: ");
+        if (login in array) {
+            while (true) {
+                let password = prompt("Введите пароль: ");
+                if (password == array[login]["password"]) {
+                    alert('Вы успешно вошли на страницу!');
+                    break;
+                } else {
+                    alert('Неправильный пароль!');
+                }
+            }
+            break;
+        } else {
+            alert('Такого пользователя не существует.');
+        }
+    }
 }
-
